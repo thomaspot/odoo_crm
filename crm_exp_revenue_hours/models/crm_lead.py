4081ac8ceac58ef7bce4e94a1a6032631d96478f
Thomas Pot <thomaspot@users.noreply.github.com>
2020-01-05 14:49:48 +0100

    Update crm_lead.py

--- crm_exp_revenue_hours/models/crm_lead.py
+++ crm_exp_revenue_hours/models/crm_lead.py
@@ -7,18 +7,26 @@ from odoo import _, api, exceptions, fields, models
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    planned_revenue_hours = fields.float(string='Planned revenue in Hours')
-    planned_revenue_product = fields.Many2many(
+    planned_revenue_hours = fields.Float(string='Planned revenue in Hours',track_visibility='onchange')
+    planned_revenue_product = fields.Many2one(
         comodel_name='product.product', string="Hour rate",
         domain="[('type', '=', 'service')]")
 
-    planned_revenue_amount = fields.float(
-        compute='_compute_planned_revenue_amount',
+    planned_revenue_amount = fields.Monetary(
+        compute='_compute_planned_revenue_amount', currency_field='company_currency',
     )
 
     @api.multi
     def _compute_planned_revenue_amount(self):
         """Calculate amount based on hours."""
         for lead in self:
-	    hourly_rate = lead.planned_revenue_product.list_price
+            hourly_rate = lead.planned_revenue_product.list_price
             lead.planned_revenue_amount = lead.planned_revenue_hours * hourly_rate
+
+    @api.onchange('planned_revenue_hours')
+    def onchange_planned_revenue_hours(self):    
+        for lead in self:
+            if lead.planned_revenue_hours > 0:
+                if lead.planned_revenue_product:
+                    hourly_rate = lead.planned_revenue_product.list_price
+                    lead.planned_revenue = lead.planned_revenue_hours * hourly_rate 
